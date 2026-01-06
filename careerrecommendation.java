741c733414926f9f4f59106dc4b3b08a7b637ca0
Adeebafatima002 <21F22324@mec.edu.om>
2026-01-06 05:44:46 +0400

    Create careerrecommendation.java

---
+++ careerrecommendation.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CareerRecommendation {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int studentCount = 5;
+
+        while (studentCount > 0) {
+
+            System.out.print("Enter GPA: ");
+            double gpa = sc.nextDouble();
+
+            if (gpa >= 3.5) {
+                System.out.println("Recommend Research or Higher Studies");
+            } else {
+                System.out.println("Recommend Industry-Oriented Career");
+            }
+
+            studentCount--;
+        }
+
+        sc.close();
+    }
+}
+  
